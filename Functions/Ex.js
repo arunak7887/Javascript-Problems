1304f0673d8bd871311654117c95e9c7769f761c
Arun Budhaji kinwad <85956491+arunak7887@users.noreply.github.com>
2024-01-25 11:46:01 +0530

    Create Ex.js

---
+++ Functions/Ex.js
@@ -0,0 +1,26 @@
+function myFunction(g1,g2){
+  return g1/g2;
+}
+const value=myFunction(8,2);
+console.log(value);
+
+//output:4
+
+
+function calAddition(number1,number2){
+  return number1+number2;
+}
+console.log(calAddition(6,9));
+
+//output:15
+
+
+square problem:
+
+const square= function (number){
+  reeturn number*number;
+};
+const x=square(4);
+console.log(x);
+
+
