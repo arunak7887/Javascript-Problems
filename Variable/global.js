65e9261cb71ad65d2ed55646f97ba0e9bd66becd
Arun Budhaji kinwad <85956491+arunak7887@users.noreply.github.com>
2024-01-25 10:41:48 +0530

    Create global.js

---
+++ Variable/global.js
@@ -0,0 +1,12 @@
+var data=200;
+function a(){
+  document.writeln(data);
+}
+function b(){
+  document.writeln(data);
+}
+a();
+b();
+
+//accessible from any function 
+//variable declared outside the function or declared with window object .
